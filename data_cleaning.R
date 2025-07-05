8c734882ce4e75b9f3fd69881346faab1895f30e
Adeeba Tak <takadeeba11@gmail.com>
2025-07-04 23:01:17 -0400

    Initial setup: added .gitignore, connected to SQLite, explored schema

---
+++ data_cleaning.R
@@ -0,0 +1,18 @@
+# Install the necessary libraries.
+install_if_needed <- function(pkg) {
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg)
+    library(pkg, character.only = TRUE, )
+  }
+}
+
+invisible(lapply(c("DBI", "RSQLite", "tidyverse", "ggplot2"), install_if_needed))
+
+# Connect to the SQLite Database.
+con <- dbConnect(RSQLite::SQLite(), "/Users/adeebatak/Desktop/Projects/icu-readmission/data/eicu_v2_0_1.sqlite3")
+
+# Tables in the SQLite Database
+tables <- dbListTables(con)
+
+print(tables)
+
