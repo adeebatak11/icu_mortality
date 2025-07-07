67f238d1f4676fcc778262ec8af637639b191e32
Adeeba Tak <takadeeba11@gmail.com>
2025-07-07 16:04:08 -0400

    Decided to use APS scores directly rather than derive them from scratch

--- scripts/01_load_data.R
+++ scripts/01_load_data.R
@@ -10,9 +10,8 @@ aps_var <- dbGetQuery(con, paste(readLines("sql/apache_APS_var.sql"), collapse =
 #load the APACHE prediction variables
 apache_var <- dbGetQuery(con, paste(readLines("sql/apache_pred_var.sql"), collapse = "\n"))
 
-
-
-
+#load the APACHE score & predictions as well as actual outcomes
+p_results <- dbGetQuery(con, paste(readLines("sql/apache_patient_results.sql"), collapse = "\n"))
 
 # used for testing
 test_table <- dbGetQuery(con, paste(readLines("sql/testing.sql"), collapse = "\n"))
